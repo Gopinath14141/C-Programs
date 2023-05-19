f1c32fcec6f5d0ab115ce4658c6d7eff080f3f73
Gopinath.R <102424775+Gopinath14141@users.noreply.github.com>
2023-05-20 00:07:34 +0530

    Update and rename main.c to voting.c

---
+++ voting.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age=18;
+    if(age>=18)
+        printf("Candidate is Eligible to Vote");
+    else
+        printf("Candidate is Ineligible to Vote");
+    return 0;
+}
