709150ff96d695c22b99845ea79b6012bec7888f
Gopinath.R <102424775+Gopinath14141@users.noreply.github.com>
2023-05-19 23:27:55 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=50,b=50;
+    int *p;int *q;
+    printf("Call by reference");
+    printf("a=%d b=%d\n", a,b);
+    return 0;
+}
+void sum (int p, int q)
+int a,b;
+{
+printf("The value is : p,q");
+p=&a;
+q=&b;
+}
+
