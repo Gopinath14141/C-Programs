583b301c6e54930e9661c0e645e878476546fd6e
Gopinath.R <102424775+Gopinath14141@users.noreply.github.com>
2023-05-19 23:41:00 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int s,e,a;
+    printf("Enter the starting range : ");
+    scanf("%d", &s);
+    printf("Enter the ending range : ");
+    scanf("%d", &e);
+    for(a=s;a<s;a++)
+        if(a%4==0)&&(a%100==0)
+    {
+        printf("Leap year %d \n", a);
+    }
+    return 0;
+}
