3ff41ba6a537d32ae1f53f4edb02c5130fa8e1b3
Gopinath.R <102424775+Gopinath14141@users.noreply.github.com>
2023-05-19 23:59:25 +0530

    Update and rename main.c to swapping_the_two_values.c

--- swapping_the_two_values.c
+++ swapping_the_two_values.c
@@ -1,22 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*const int MAX = 3;
-int main()
-{
-    int var[] = {10,100,200};
-    int i, *ptr[MAX];
-    for(i=0;i<MAX;i++)
-    {
-        ptr[i]=&var[i];
-    }
-    for(i=0;i<MAX;i++)
-    {
-        printf("Value of var[%d] = %d\n", i,*ptr[i]);
-    }
-    return 0;
-}*/
-
 //int Call_by_Reference();
 int Call_by_Value();
 int main()
